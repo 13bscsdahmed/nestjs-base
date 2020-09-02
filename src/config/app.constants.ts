31b80b69325f85c794b57b3712325085a43210d1
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2020-09-02 17:46:05 +0500

    Set global prefix of api/v1 in routes

--- src/config/app.constants.ts
+++ src/config/app.constants.ts
@@ -1,4 +1,5 @@
 export const appConstants = {
+  appPrefix: 'api/v1',
   providers: {
     DB_PROVIDER: 'db_connection',
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { LogService } from './common/modules/logger/services';
 import { requestMiddleware } from './common/modules/shared/middlewares/request.middleware';
+import { appConstants } from './config/app.constants';
 
 async function bootstrap() {
   let app;
@@ -10,12 +11,19 @@ async function bootstrap() {
   let logService: LogService;
   try {
     app = await NestFactory.create(AppModule);
+    
+    // Setting app prefix
+    app.setGlobalPrefix(appConstants.appPrefix);
+    
     configService = app.get(ConfigService);
     logService = app.get(LogService);
     logService.info(undefined, 'Server initialized successfully');
+    // Use global request middleware
     app.use(requestMiddleware);
+    
     await app.listen(configService.get('PORT') || 3000);
     logService.info(undefined, `Server started on port: ${configService.get('PORT') || 3000}`);
+    
   } catch (error) {
     logService.error(undefined, `App error occurred on port: ${configService.get('PORT') || 3000}`);
     process.exit(1);
